9f221e110515cee5a6d4c18342153fdc2b5ffe55
Tony <tony.servanin@gmail.com>
2025-07-03 18:54:38 +0200

    add: Component imported into main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import krma from "@/assets/logo/Wordmark.svg";
-import Image from "next/image";
+import LandingSection from "@/components/containers/homepage/LandingSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <div className="container pl-16 py-48 gap-11">
-        <Image src={krma} alt="krma" width={432} height={169} />
-        <h2 className="max-w-xl text-center">
-          L’agence de développement qui place vos intérêts avant les siens
-        </h2>
+        <LandingSection />
       </div>
     </main>
   );
