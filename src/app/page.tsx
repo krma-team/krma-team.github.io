02cc929246be7ce2d9930824a565080bc66000d9
Tony <tony.servanin@gmail.com>
2025-07-03 10:31:46 +0200

    update: light changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import krma from '@/assets/logo/Wordmark.svg';
-import Image from 'next/image';
+import krma from "@/assets/logo/Wordmark.svg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className="container pl-16 gap-11">
+      <div className="container pl-16 py-20 gap-11">
         <Image src={krma} alt="krma" width={432} height={169} />
         <h2 className="max-w-xl text-center">
           L’agence de développement qui place vos intérêts avant les siens
