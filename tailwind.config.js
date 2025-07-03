dff3e009fe5f78e65d0d66417f0a48ff75983406
Tony <tony.servanin@gmail.com>
2025-07-03 10:32:51 +0200

    update: config change to point tailwind to the containers/ folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        color: 'hsl(var(--text))',
+        background: "hsl(var(--background))",
+        color: "hsl(var(--text))",
 
         // Couleurs de marque
-        'brand-lime': 'hsl(var(--brand-lime))',
-        'brand-lavender': 'hsl(var(--brand-lavender))',
-        'brand-beige': 'hsl(var(--brand-beige))',
-        'brand-powder': 'hsl(var(--brand-powder))',
-        'brand-giants': 'hsl(var(--brand-giants))',
+        "brand-lime": "hsl(var(--brand-lime))",
+        "brand-lavender": "hsl(var(--brand-lavender))",
+        "brand-beige": "hsl(var(--brand-beige))",
+        "brand-powder": "hsl(var(--brand-powder))",
+        "brand-giants": "hsl(var(--brand-giants))",
       },
     },
   },
