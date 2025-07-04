ca78390add912ec3204269f2b9194bbb68d33e63
Tony <tony.servanin@gmail.com>
2025-07-04 12:11:39 +0200

    feature: adding font SYNE with tailwind4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
-import { DM_Mono } from "next/font/google";
+import { DM_Mono, Syne } from "next/font/google";
 import "@/assets/styles/globals.css";
 import Header from "@/components/Header";
 
-const dm_mono = DM_Mono({ weight: ["300", "400", "500"], subsets: ["latin"] });
+const dm_mono = DM_Mono({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+});
+const syne = Syne({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-syne",
+});
 
 export const metadata: Metadata = {
   title: "KRMA",
@@ -17,7 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${dm_mono.className} bg-background text-foreground`}>
+      <body
+        className={`${dm_mono.className} ${syne.variable} bg-background text-foreground`}
+      >
         <Header />
         {children}
       </body>
